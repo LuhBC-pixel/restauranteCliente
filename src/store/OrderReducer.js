08a7bdd387f4477c5f27edbdb265405679a6369f
LuisaBC-pixel <bc.luisa@yahoo.com.br>
2022-04-08 11:33:00 -0300

    update orders

--- src/store/OrderReducer.js
+++ src/store/OrderReducer.js
@@ -1,6 +1,11 @@
 const INITIAL_STATE = {
   menu: [],
-  orders: [],
+  order: {
+    items: [],
+    paymentType: '',
+    status: '',
+    placeId: {}
+  },
   isRequest: false,
   isPay: false,
   statusRequest: '',
@@ -13,7 +18,7 @@ export const ORDER_TYPES = {
 export const OrderReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ORDER_TYPES.MENU_LOADED:
-      return { ...state,  menu: action.payload};
+      return { ...state, menu: action.payload };
     default:
       return { ...state };
   }
